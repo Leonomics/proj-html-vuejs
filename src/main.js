45281a9b5c45407759abea22c50a795c74d880a5
Leonomics <leocv97@gmail.com>
2022-09-14 19:50:00 +0200

    add imgs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret, faChevronDown, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret, faChevronDown, faCartShopping)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
 Vue.config.productionTip = false
 
 new Vue({
